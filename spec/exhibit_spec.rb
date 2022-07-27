49705600dc3537f692802347ff258bfc0f081fe0
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-07-27 09:26:14 -0600

    add test for remainder of iteration 1

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -3,7 +3,7 @@ require './lib/patron'
 
 RSpec.describe Exhibit do
   let(:exhibit) {described_class.new({name: "Gems and Minerals", cost: 0})}
-
+  let(:patron_1) {Patron.new("Bob", 20)}
   it 'is an instance of exhibit' do
     expect(exhibit).to be_an_instance_of(Exhibit)
   end
@@ -14,6 +14,29 @@ RSpec.describe Exhibit do
 
   xit 'has a cost' do
     expect(exhibit.cost).to eq 0
+  end
+
+  it 'has patrons' do
+    expect(patron_1).to be_an_instance_of(Patron)
+  end
+
+  xit 'patron has a name' do
+    expect(patron_1.name).to eq "Bob"
+  end
+
+  xit 'patron has spending money' do
+    expect(patron_1.spending_money).to eq 20
+  end
+
+  xit 'patron has no interests by default' do
+    expect(patron_1.interests).to eq []
+  end
+
+  xit 'patron can add interests' do
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    
+    expect(patron_1.interests).to eq ["Dead Sea Scrolls", "Gems and Minerals"]
   end
   
 end
